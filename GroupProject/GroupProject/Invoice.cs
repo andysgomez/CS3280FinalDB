35c4b945adec422e6afdbb5ce24464e2f93ac31b
Aaron Mortensen <aaronmortensen@mail.weber.edu>
2019-04-05 16:50:54 -0600

    Invoice class is functional, feel free to expand it to fit your needs

---
+++ GroupProject/GroupProject/Invoice.cs
@@ -0,0 +1,148 @@
+﻿using GroupProject.Main;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject
+{
+    /// <summary>
+    /// A class to hold data for Invoice objects
+    /// </summary>
+    public class Invoice
+    {
+        /// <summary>
+        /// The invoice number
+        /// </summary>
+        private int invoiceNumber;
+
+        /// <summary>
+        /// the date of the invoice
+        /// </summary>
+        private DateTime invoiceDate;
+
+        /// <summary>
+        /// the total cost of the invoice
+        /// </summary>
+        private double totalCost;
+
+        /// <summary>
+        /// the list of items on the invoice
+        /// </summary>
+        private BindingList<Item> items;
+
+
+        public int InvoiceNumber { get => invoiceNumber; set => invoiceNumber = value; }
+        public DateTime InvoiceDate { get => invoiceDate; set => invoiceDate = value; }
+        public double TotalCost { get => totalCost; set => totalCost = value; }
+        public BindingList<Item> Items { get => items; set => items = value; }
+
+
+        /// <summary>
+        /// blank constructor for an invoice
+        /// </summary>
+        public Invoice()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name +
+                    "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// constructor that takes in all data.
+        /// use this with the sql classes
+        /// </summary>
+        /// <param name="invoiceNumber"></param>
+        /// <param name="date"></param>
+        /// <param name="totalCost"></param>
+        /// <param name="items"></param>
+        public Invoice(int invoiceNumber, DateTime date, double totalCost, BindingList<Item> items)
+        {
+            try
+            {
+                InvoiceNumber = invoiceNumber;
+                this.InvoiceDate = date;
+                TotalCost = totalCost;
+                Items = items;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name +
+                    "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// public method to add an item to the invoice
+        /// recalculates the total
+        /// </summary>
+        /// <param name="item"></param>
+        public void addItemToInvoice(Item item)
+        {
+            try
+            {
+                Items.Add(item);
+                calculateTotal();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name +
+                    "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Helper method to calculate the total cost of on invoice
+        /// also updates this invoices cost
+        /// </summary>
+        /// <returns></returns>
+        private double calculateTotal()
+        {
+            try
+            {
+                double temp = 0.0;
+
+                foreach (Item item in Items)
+                {
+                    temp += item.ItemCost;
+                }
+                TotalCost = temp;
+                return temp;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name +
+                    "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// gets the total of the invoice
+        /// </summary>
+        /// <returns></returns>
+        public double getTotal()
+        {
+            try
+            {
+                return calculateTotal();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name +
+                    "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        
+    }
+}
