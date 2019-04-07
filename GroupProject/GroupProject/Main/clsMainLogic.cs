b688ef342abea9a74f4e96b93833d09cfa41ca02
Aaron Mortensen <aaronmortensen@mail.weber.edu>
2019-04-06 18:39:13 -0600

    Final prototype commit

--- GroupProject/GroupProject/Main/clsMainLogic.cs
+++ GroupProject/GroupProject/Main/clsMainLogic.cs
@@ -8,6 +8,12 @@ using System.Threading.Tasks;
 
 namespace GroupProject.Main
 {
+    //For sharing data between windows the logic classes will have getMethods that return and data structures
+    //the other classes might need, and to indicate that changes have been made or backed out of, we plan to use
+    //boolean flags that are set and can be checked from the other windows...
+    //so like if one of the windows changes the database, it will set a flag indicating that its been changed
+    //so the returning window can reload its data
+
     /// <summary>
     /// All the logic behind the main window
     /// </summary>
