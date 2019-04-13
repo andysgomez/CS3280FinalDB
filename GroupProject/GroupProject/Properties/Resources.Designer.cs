da52a8c50826bab71c6801cd1940ce7261b0cb3a
Andrew Maynez <andysgomez@hotmail.com>
2019-04-13 16:27:30 -0600

    Changed startup location of window (centered) background color font size border thickness font family button background color on all windows.

--- GroupProject/GroupProject/Properties/Resources.Designer.cs
+++ GroupProject/GroupProject/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace GroupProject.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap hermes {
-            get {
-                object obj = ResourceManager.GetObject("hermes", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
